d4273e0378a941b4ab6c76591cda4e7f10f61736
Matthew Ketas <matt.ketas@gmail.com>
2026-04-02 15:29:48 -0400

    Initial Commit

---
+++ FlicFinder/FlicFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlicFinderApp.swift
+//  FlicFinder
+//
+//  Created by Matthew Ketas on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct FlicFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
